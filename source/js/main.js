00a5483cc7b5c0c51ecac1dfd725a368d27f412f
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-10-26 16:25:30 +0100

    Initial commit with jQuery example

---
+++ source/js/main.js
@@ -0,0 +1,29 @@
+$('h1').hide()
+
+$('a').on('click', function (e){
+  var header = $('h1')
+
+  if($('h1:visible').length > 0) {
+    header.hide()
+  } else {
+    header.show()
+  }
+
+
+  e.preventDefault()
+})
+
+
+
+$('button.make-blue').on('click', function (e) {
+  var div = $('div.blue')
+  $.ajax({
+      url: 'https://api.github.com/users/pepe',
+      success: function (data) {
+        $('div.blue').html(data.email)
+        div.css('background', 'blue')
+      }
+  })
+  div.animate({right: '0'}, 1000)
+})
+
